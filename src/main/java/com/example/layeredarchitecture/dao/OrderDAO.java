60631caac04baf6c9ba901fbfcf1dee69d5c010d
harsha2531 <harshanimeda@gmail.com>
2024-06-15 20:06:55 +0530

    Created Persistant Layer

---
+++ src/main/java/com/example/layeredarchitecture/dao/OrderDAO.java
@@ -0,0 +1,15 @@
+package com.example.layeredarchitecture.dao;
+
+import com.example.layeredarchitecture.db.DBConnection;
+import com.example.layeredarchitecture.model.OrderDTO;
+import com.example.layeredarchitecture.model.OrderDetailDTO;
+
+import java.sql.*;
+import java.util.List;
+
+public interface OrderDAO {
+
+    public String lastOrderId() throws SQLException, ClassNotFoundException;
+    public boolean checkOrderIdExists(String orderId) throws SQLException, ClassNotFoundException;
+    public boolean saveOrder(OrderDTO orderDTO, List<OrderDetailDTO> orderDetails) throws SQLException, ClassNotFoundException;
+}
